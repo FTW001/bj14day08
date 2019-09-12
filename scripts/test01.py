51344a3e232f6c069eec1a2ffbc76d8e1e9dd1a7
FTW001 <fw13546440407@163.com>
2019-09-12 11:31:44 +0800

    Initial commit

---
+++ scripts/test01.py
@@ -0,0 +1,13 @@
+class Test:
+    def test1(self):
+        print("test1被执行")
+
+    def test2(self):
+        print("test2被执行")
+
+    def test3(self):
+        print("test3被执行")
+        return 0
+
+
+
